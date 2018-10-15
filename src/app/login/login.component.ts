b5a3e2865ab0d1b8e5d9ced2f3312a70c287cc27
Sandra Barkhuizen <sandrabarkhuizen@2degreesnetwork.com>
2018-10-15 13:46:25 +0100

    Added animation and organised styles.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+// import fade in animation
+import { fadeInAnimation } from '../_animations/index';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  // make fade in animation available to this component
+  animations: [fadeInAnimation],
+  
+  // attach the fade in animation to the host (root) element of this component
+  host: { '[@fadeInAnimation]': '' }
 })
 export class LoginComponent implements OnInit {
 
--- src/app/reset/reset.component.ts
+++ src/app/reset/reset.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeInAnimation } from '../_animations/index';
 
 @Component({
   selector: 'app-reset',
   templateUrl: './reset.component.html',
-  styleUrls: ['./reset.component.scss']
+  styleUrls: ['./reset.component.scss'],
+  animations: [fadeInAnimation],
+  host: { '[@fadeInAnimation]': '' }
 })
 export class ResetComponent implements OnInit {
 
